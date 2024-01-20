4a2809fe9e7208638e5ecc37e2c86a901e55ba6e
Pedro Henrique <phsantos021@gmail.com>
2024-01-20 19:12:14 -0300

    feat logs erros

--- src/useCases/create-user/create.user.controller.ts
+++ src/useCases/create-user/create.user.controller.ts
@@ -1,16 +1,18 @@
 import { Request, Response } from 'express'
 import { CreateUserUseCase } from './create-user.usecase'
+import { logger } from '../../utils/logger'
+import { errorMonitor } from 'stream'
 
 export class CreateUserController {
   async handle(request: Request, response: Response) {
+    logger.info('Usuario sendo criado !')
     try {
       const data = request.body
-
       const userCase = new CreateUserUseCase()
       const result = await userCase.execute(data)
-
       return response.json(result)
     } catch (error: any) {
+      logger.error(`${error.message}\n${error.stack}`)
       return response.status(400).json(error.message)
     }
   }
